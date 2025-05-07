39c2d484818e567083c25c66ddca8a9ccf60c62a
nikhil-solanki <nikhilsolanki02549@gmail.com>
2025-05-08 00:53:27 +0530

    Group Middlerware

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,14 @@ Route::view('/login','login')->name('login');
 Route::view('/register','register')->name('register');
 Route::post('/registersave',[UserController::class,'register'])->name('registersave');
 Route::post('/loginmatch',[UserController::class,'login'])->name('loginmatch');
-Route::get('/dashboard',[UserController::class,'dashboard'])->name('dashboard')->middleware('IsUserValid',TestUser::class);  
-Route::get('/innerpage',[UserController::class,'innerpage'])->name('innerpage')->middleware('IsUserValid');
+// Route::get('/dashboard',[UserController::class,'dashboard'])->name('dashboard')->middleware('IsUserValid',TestUser::class);  
+// Route::get('/innerpage',[UserController::class,'innerpage'])->name('innerpage')->middleware('IsUserValid');
+
+
+Route::middleware(['IsUserValid',TestUser::class])->group(function () {
+    Route::get('/dashboard',[UserController::class,'dashboard'])->name('dashboard');  
+    Route::get('/innerpage',[UserController::class,'innerpage'])->name('innerpage');
+});
 Route::get('/logout',[UserController::class,'logout'])->name('logout');
 
     
